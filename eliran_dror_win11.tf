74b7bf30ec4ba73055c305e2eb29b0b2ce4b9a20
Eliran Dror <eldror@paloaltonetworks.com>
2025-01-20 13:51:30 +0200

    Make TF misconfigured

--- eliran_dror_win11.tf
+++ eliran_dror_win11.tf
@@ -50,17 +50,24 @@ resource "aws_security_group" "windows11_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 3389
-    to_port     = 3389
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow RDP access from anywhere (secure this in production)
+    cidr_blocks = ["0.0.0.0/0"] # Allow all inbound traffic (insecure)
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all inbound traffic (insecure)
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 
   tags = {
@@ -70,12 +77,12 @@ resource "aws_security_group" "windows11_sg" {
 
 resource "aws_key_pair" "main" {
   key_name   = "windows11-key"
-  public_key = file("~/.ssh/id_rsa.pub") # Replace with your public key path
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA7fPfB4M1J3RWRv/zGIPeIvdfLTYZ2lf9R2hcIoAAAAADAQAB" # Public key hardcoded (insecure)
 }
 
 resource "aws_instance" "windows11" {
   ami           = "ami-0d8f6eb4f641ef691" # Update to a Windows 11 AMI ID available in your region
-  instance_type = "t2.medium" # Use a suitable instance type for Windows 11
+  instance_type = "t2.micro" # Use a minimal instance type (potential performance issues)
   key_name      = aws_key_pair.main.key_name
   subnet_id     = aws_subnet.main.id
   security_groups = [aws_security_group.windows11_sg.name]
@@ -86,9 +93,8 @@ resource "aws_instance" "windows11" {
 
   user_data = <<-EOF
               <powershell>
-              # Enable RDP and other initial configurations
-              Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name "fDenyTSConnections" -Value 0
-              Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+              # Disable Windows Firewall (insecure)
+              Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
               </powershell>
               EOF
 }
